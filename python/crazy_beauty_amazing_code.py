0605af039c06242febca0588b3b105149a2052cb
pana <pana.wang@outlook.com>
2012-11-18 13:13:47 +0800

    the first beauty code

---
+++ python/crazy_beauty_amazing_code.py
@@ -0,0 +1,38 @@
+# The most crazy beauty amazing python code i ever meet
+_                                      =   (
+                                        255,
+                                      lambda
+                               V       ,B,c
+                             :c   and Y(V*V+B,B,  c
+                               -1)if(abs(V)<6)else
+               (              2+c-4*abs(V)**-0.4)/i
+                 )  ;v,      x=1500,1000;C=range(v*x
+                  );import  struct;P=struct.pack;M,\
+            j  ='<QIIHHHH',open('M.bmp','wb').write
+for X in j('BM'+P(M,v*x*3+26,26,12,v,x,1,24))or C:
+            i  ,Y=_;j(P('BBB',*(lambda T:(T*80+T**9
+                  *i-950*T  **99,T*70-880*T**18+701*
+                 T  **9     ,T*i**(1-T**45*2)))(sum(
+               [              Y(0,(A%3/3.+X%v+(X/v+
+                               A/3/3.-x/2)/1j)*2.5
+                             /x   -2.7,i)**2 for  \
+                               A       in C
+                                      [:9]])
+                                        /9)
+                                       )   )
+
+
+
+'''
+  these code renders the Mandelbrot set as a full-color, anti-aliased 1500x1000 image
+
+  no third-party libraries are required - just pure python. however only on python 2.5-2.7
+
+  the output is written to M.bmp(Windows bitmap format).
+
+  the time it takes is a little long -- 18 minutes on 1.86 GHz Core 2 Duo (or 9 minutes using PyPy)
+
+  you can change the image resolution at line 9 (Just make sure the width is divisible by 4.) 
+
+  check the detail infomation at http://preshing.com/20110926/high-resolution-mandelbrot-in-obfuscated-python
+'''
